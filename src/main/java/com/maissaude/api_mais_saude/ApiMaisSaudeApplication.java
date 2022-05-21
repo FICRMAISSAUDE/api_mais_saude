f23573897031fd30db27c767c8058cbc394abf56
Vinicius Lima <viniciusmblimma@gmail.com>
2022-05-20 21:08:25 -0300

    arquivos iniciais do spring

---
+++ src/main/java/com/maissaude/api_mais_saude/ApiMaisSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.maissaude.api_mais_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMaisSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMaisSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maissaude/api_mais_saude/ApiMaisSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maissaude.api_mais_saude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMaisSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
